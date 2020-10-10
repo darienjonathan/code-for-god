d18e1f5c7304e8d4d8e367b60067ddd2fe0f9e09
darienjonathan <darien-winata@kayac.com>
2020-10-10 11:35:00 +0900

    implement keluarga

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Keluarga from "@/views/Keluarga.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Keluarga",
+    component: Keluarga
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/keluarga",
+    name: "Keluarga",
+    component: Keluarga
   }
 ];
 
