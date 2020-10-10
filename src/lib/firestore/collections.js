d18773e5906380d929407d571d3fff3186ce3440
darienjonathan <darien-winata@kayac.com>
2020-10-10 11:34:29 +0900

    firestore refactor

--- src/lib/firestore/collections.js
+++ src/lib/firestore/collections.js
@@ -1,3 +1,6 @@
 import Firestore from "@/lib/firestore/Firestore";
 
 export const keluargaCollection = new Firestore("keluarga");
+
+export const keluargaContentCollection = keluargaId =>
+  new Firestore(`keluarga/${keluargaId}/contents`);
