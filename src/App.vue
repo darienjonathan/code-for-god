3b6117fcd4ed66908504fc6d7414f6d078bf43c8
darienjonathan <darien-winata@kayac.com>
2020-10-10 14:38:52 +0900

    keluarga item

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,37 @@
 </template>
 
 <script>
+import { keluargaCollection, keluargaContentCollection } from "@/lib/firestore/collections";
+import dayjs from "@/lib/dayjs";
+import { mapMutations } from "vuex";
+
 export default {
   name: "App",
-
   data: () => ({
     //
-  })
+  }),
+  async mounted() {
+    const keluargaDocs = await keluargaCollection.loadCollection({
+      orderBy: ["ts", "asc"]
+    });
+    const contentDocs = await Promise.all(
+      keluargaDocs.map(([uid, doc]) =>
+        keluargaContentCollection(uid).loadCollection({
+          orderBy: ["order", "asc"]
+        })
+      )
+    );
+    keluargaDocs.map(([uid, doc], index) => {
+      const time = dayjs.unix(doc.ts.seconds);
+      doc.ts.str = time.format("dddd, D MMMM YYYY");
+      doc.ts.dateNum = `Hari ${index + 1}`;
+      doc.contents = contentDocs[index];
+    });
+    this.setItems(keluargaDocs);
+  },
+  methods: {
+    ...mapMutations("keluarga", ["setItems"])
+  }
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Keluarga from "@/views/Keluarga.vue";
+import KeluargaItem from "@/views/KeluargaItem.vue";
 
 Vue.use(VueRouter);
 
@@ -14,12 +15,12 @@ const routes = [
     path: "/keluarga",
     name: "Keluarga",
     component: Keluarga
+  },
+  {
+    path: "/keluarga/:id",
+    name: "KeluargaItem",
+    component: KeluargaItem
   }
-  // {
-  //   path: "/keluarga/:id",
-  //   name: "KeluargaContent",
-  //   component: KeluargaContent
-  // }
 ];
 
 const router = new VueRouter({
