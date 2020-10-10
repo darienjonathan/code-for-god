b26ab482bb1445dca8e1d981aad06af2d0d7d70c
darienjonathan <darien-winata@kayac.com>
2020-10-10 17:00:06 +0900

    breadcrumbs

---
+++ src/lib/upperCaseFirstLetter.js
@@ -0,0 +1,5 @@
+export default str =>
+  str
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
--- src/views/KeluargaItem.vue
+++ src/views/KeluargaItem.vue
@@ -5,6 +5,15 @@
         <h1 class="d.flex text-center">{{ item.ts.dateNum }}</h1>
         <h2 class="d.flex text-center">{{ item.ts.str }}</h2>
       </div>
+      <v-breadcrumbs :items="paths">
+        <template v-slot:item="{ item }">
+          <v-breadcrumbs-item>
+            <router-link :to="item.to">
+              {{ item.url }}
+            </router-link>
+          </v-breadcrumbs-item>
+        </template>
+      </v-breadcrumbs>
       <v-expansion-panels>
         <v-expansion-panel v-for="[uid, content] in item.contents" :key="uid">
           <v-expansion-panel-header>{{ content.title }}</v-expansion-panel-header>
@@ -23,6 +32,7 @@
 import { mapGetters } from "vuex";
 import Wordpress from "@/components/Wordpress";
 import { getPost } from "@/api/index";
+import upperCaseFirstLetter from "@/lib/upperCaseFirstLetter.js";
 export default {
   name: "KeluargaItem",
   components: {
@@ -30,28 +40,49 @@ export default {
   },
   data() {
     return {
+      id: undefined,
       item: undefined,
       paths: []
     };
   },
   async mounted() {
-    const id = this.$route.params.id;
-    const item = this.getItem(id);
-    if (!item) {
-      return;
-    }
-    const postPromises = item.contents.map(
-      ([uid, content]) => content.postId && getPost(content.postId)
-    );
-    const posts = await Promise.all(postPromises);
-    posts.forEach((post, index) => {
-      item.contents[index][1].post = post;
-    });
-    console.log({ item, posts });
-    this.item = item;
+    this.id = this.$route.params.id;
+    await this.prepareItem();
+    this.preparePath();
   },
   computed: {
     ...mapGetters("keluarga", ["getItem"])
+  },
+  methods: {
+    preparePath() {
+      if (!this.item || !this.id) {
+        return;
+      }
+      const splittedPaths = this.$route.path.split("/").filter(path => path.length);
+      const paths = splittedPaths.map((path, index) => ({
+        to: "/" + splittedPaths.slice(0, index + 1).join("/"),
+        url: path === this.id ? this.item.ts.dateNum : upperCaseFirstLetter(path)
+      }));
+      console.log(paths);
+      this.paths = paths;
+    },
+    async prepareItem() {
+      if (!this.id) {
+        return;
+      }
+      const item = this.getItem(this.id);
+      if (!item) {
+        return;
+      }
+      const postPromises = item.contents.map(
+        ([uid, content]) => content.postId && getPost(content.postId)
+      );
+      const posts = await Promise.all(postPromises);
+      posts.forEach((post, index) => {
+        item.contents[index][1].post = post;
+      });
+      this.item = item;
+    }
   }
 };
 </script>
